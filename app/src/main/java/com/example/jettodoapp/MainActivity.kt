3fa366201d49b16ae37f0a69d61991c3d11a982d
John-Thailand <naoki.honda@mirrorfit.jp>
2024-11-18 19:30:57 +0900

    TODOアプリ実装

--- app/src/main/java/com/example/jettodoapp/MainActivity.kt
+++ app/src/main/java/com/example/jettodoapp/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.jettodoapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.jettodoapp.components.EditDialog
+import com.example.jettodoapp.components.TaskList
 import com.example.jettodoapp.ui.theme.JetTodoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,9 +35,34 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MainContent()
                 }
             }
         }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainContent(viewModel: MainViewModel = hiltViewModel()) {
+    if (viewModel.isShowDialog) {
+        EditDialog()
+    }
+
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = { viewModel.isShowDialog = true }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "新規作成")
+        }
+    }) {
+        val tasks by viewModel.tasks.collectAsState(initial = emptyList())
+
+        TaskList(
+            tasks = tasks,
+            onClickRow = {
+                viewModel.setEditingTask(it)
+                viewModel.isShowDialog = true
+            },
+            onClickDelete = { viewModel.deleteTask(it) },
+        )
     }
 }
